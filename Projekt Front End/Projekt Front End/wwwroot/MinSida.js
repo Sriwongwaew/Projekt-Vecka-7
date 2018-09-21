55d70fc4ea66ec47d00737818d01b06ac269974a
LAPTOP-96UQLCFD\sriwo <sriwongwaew@gmail.com>
2018-09-21 11:02:16 +0200

    Updated

--- Projekt Front End/Projekt Front End/wwwroot/MinSida.js
+++ Projekt Front End/Projekt Front End/wwwroot/MinSida.js
@@ -1,1 +1,29 @@
-﻿
+﻿let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+
+let loginbutton = document.getElementById("knapp1");
+
+button1.addEventListener("click", () => {
+    window.location.href = "Minsida2.html";
+})
+
+button2.addEventListener("click", () => {
+    window.location.href = "Minsida2.html";
+})
+
+
+button3.addEventListener("click", () => {
+    window.location.href = "Minsida2.html";
+})
+
+
+button4.addEventListener("click", () => {
+    window.location.href = "Minsida2.html";
+})
+
+loginbutton.addEventListener("click", () => {
+    window.location.href ="Minsida2.html"
+})
+
